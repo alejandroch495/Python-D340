0a871c1dca5aece79af2a7147e04ea5853b992a9
Alex C <AlejandroCh495@gmail.com>
2025-04-16 08:34:31 -0700

    -Added variables that need to be defined -Broke down code bit by bit to write code in a much more managable way

--- Ch2_Ex1.py
+++ Ch2_Ex1.py
@@ -9,8 +9,20 @@ Enter Rate: 2.75
 Pay: 96.25
 Put at least two outputs (results after you run your code) at the end of your code as a multi-line comment.
 -----------------------------------------------------
+
+Initial scope variables needed
+
+hours => float
+rate => float
+pay => float
+
 """
 
+#Need a way to get hours
+#Need a way to get rate
+#Need to calculate pay based on hours x rate
+#Need to display pay
+#Extra: Want to display to user the "rate" and "hours"
 
  
 
