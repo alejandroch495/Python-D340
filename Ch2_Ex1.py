61cd63f6ddecb603862cc7ed1e6379dfec25bb92
Alex C <AlejandroCh495@gmail.com>
2025-04-16 08:27:56 -0700

    -Created new file for Chapter 2 exercise 1 -Added details for requirements

---
+++ Ch2_Ex1.py
@@ -0,0 +1,21 @@
+# Chapter 2 Exercise 1 - Alejandro Chavez
+# Pay Rate Calculator
+"""
+The following are the requirments for the program
+----------------------------------------------------
+Write a program to prompt the user for hours and rate per hour to compute gross pay.
+Enter Hours: 35
+Enter Rate: 2.75
+Pay: 96.25
+Put at least two outputs (results after you run your code) at the end of your code as a multi-line comment.
+-----------------------------------------------------
+"""
+
+
+ 
+
+
+ 
+
+ 
+
