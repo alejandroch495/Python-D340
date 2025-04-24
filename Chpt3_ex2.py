c2d8d70002eb293bd16f29fa18b6b1fc31c99579
Alex C <AlejandroCh495@gmail.com>
2025-04-23 17:04:57 -0700

    -Added chapter 2 exercise to modify for new program modification

---
+++ Chpt3_ex2.py
@@ -0,0 +1,23 @@
+#Variable defined
+payHours = float # Hours worked
+payRate = float # Rate paid per hour
+OVERTIME_RATE = 1.5
+payOvertime = float
+pay = float # The final calculation of hour x rate
+
+#Need a way to get hours and rate
+payHours = float(input("Enter number of hours: "))
+payRate = float(input("Enter current rate: "))
+
+#if over 40 hours worked, extract extra hours worked and multiply by overtime rate
+if payHours > 40:
+    overtimeHours = payHours - 40 
+    payOvertime = overtimeHours * (payRate * OVERTIME_RATE)
+    pay = (40 * payRate) + payOvertime
+
+else:
+    #Need to calculate pay based on hours x rate if hours is not over 40 hours
+    pay = payHours * payRate
+
+#Need to display pay
+print("\n Your current pay is \n",pay)
