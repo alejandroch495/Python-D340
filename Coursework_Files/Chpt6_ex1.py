46dbed17a37036337ac9c7a08d6534830fc1ee3b
Alex C <AlejandroCh495@gmail.com>
2025-05-28 09:40:09 -0700

    variables
    - Added two variables

--- Coursework_Files/Chpt6_ex1.py
+++ Coursework_Files/Chpt6_ex1.py
@@ -3,3 +3,5 @@
 # Friend list
 
 
+friend_list_2017 : list[(str,int)] = [("Bob",2017),("Tim",2017),("Brandon",2017)]
+friend_list_2018 : list[(str,int)] = [("Jeff",2018),("Brandon",2018),("Angel",2018)] 
