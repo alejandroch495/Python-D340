633c9642849201531970bee3f93179f559fb8f55
Alex C <AlejandroCh495@gmail.com>
2025-06-09 13:36:24 -0700

    uploaded chapter 6 exercise 1

--- Coursework_Files/Chpt6_ex1.py
+++ Coursework_Files/Chpt6_ex1.py
@@ -7,20 +7,28 @@ friend_list_2017 : list[(str,int)] = [("Bob",2017),("Tim",2017),("Brandon",2017)
 friend_list_2018 : list[(str,int)] = [("Jeff",2018),("Brandon",2018),("Angel",2018)] 
 
 
+def get_input():
+    try:
+        user_input = input("Enter friend name: " , end="")
+    except Exception as e:
+        ...
 
 
 def display_friends() -> None:
-    print("| Name     | Year |")
+    name: str 
+    year : int
+    name_spacer : int
+    print("  Name       Year  ")
     for i in friend_list_2017:
         
-        name : str = i[0]
-        year : int = i[1]
-        name_spacer : int = 9 - len(name)
+        name = i[0]
+        year = i[1]
+        name_spacer = 9 - len(name)
         print(f"| {name}{' ' * name_spacer}| {year:4.0f} |") 
     for i in friend_list_2018:
         
-        name : str = i[0]
-        year : int = i[1]
-        name_spacer : int = 9 - len(name)
+        name = i[0]
+        year = i[1]
+        name_spacer = 9 - len(name)
         print(f"| {name}{' ' * name_spacer}| {year:4.0f} |") 
 display_friends()
