71272a14560f1c3ea9e8131ae9cc03eb3d1669b7
Alex C <AlejandroCh495@gmail.com>
2025-06-11 16:22:42 -0700

    Added friends list

---
+++ Coursework_Files/Chpt6_ex2.py
@@ -0,0 +1,22 @@
+# Alejandro Chavez
+# Chapter 6 exercise 2
+# Names - using dictionaries
+
+"""
+-Create a dictionary of your friends that you met in 2017 and 2018. So, the keys are '2017' and '2018' and the values are the list of the friends. 
+"""
+friends_list: dict = {"2017" : ["Jim", "Darryl", "Jen","Bob", "Jill"], "2018" : ["Tim", "Jack", "Brandon", "Jessica", "Matthew", "Blake"]}
+
+"""
+-Print the friends' names in the dictionary 
+
+-Ask the user to enter the name of a friend.
+
+-Then show that friend you met in what year, 2017 or 2018.
+
+-Ask the user to enter a new friend name to be added to the 2017 or 2018 lists (You need also to ask the user what year they want to add the friend's name).
+
+-Then add the entered friend's name to the dictionary( to the value of the 2017 key or the value of the 2018 key)  
+
+-Again, print the dictionary.
+"""
